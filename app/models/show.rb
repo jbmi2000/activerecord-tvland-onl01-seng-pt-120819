c71dcdb6caf0dd23d369f523d576090cbfbc1b90
Jeffrey Marbury <jeff.marbury@gmail.com>
2020-03-15 19:07:38 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,11 +2,11 @@ class Show < ActiveRecord::Base
 
   has_many :characters
   belongs_to :network
-  belongs_to :actors
 
   def actors_list
     self.actors.map do |t|
      t.full_name
     end
+  end
 
 end
