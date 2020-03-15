1ab0119ca1e6aafbae747b9b9103a339e7f5881b
Jeffrey Marbury <jeff.marbury@gmail.com>
2020-03-15 17:56:30 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -5,7 +5,7 @@ class Character < ActiveRecord::Base
     belongs_to :actor
 
   def say_that_thing_you_say
-    return "#{self.name} always says: #{self.catchphrase}"
+    "#{self.name} always says: #{self.catchphrase}"
   end
 
 end
